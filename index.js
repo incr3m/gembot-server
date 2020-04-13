339968713e0ec8bc81a81c2a21922d3a8ccdd879
Your Name <you@example.com>
2020-04-13 20:26:52 +0800

    dual

--- index.js
+++ index.js
@@ -6,19 +6,23 @@ const sortBy = require("lodash/sortBy");
 const DEFAULT_TILE_SIZE_Y = 30;
 const DEFAULT_TILE_SIZE_X = 36;
 
-const DEFAULT_MAP = "moc_fild12";
+const DEVICE_MAPPING = {
+  "8006baab5f937fd8": { map: "gef_fild13", hunt: ["creamy", "poporing"] }, // redmi go,
+  "38bf9feada257016": { map: "gef_fild04", hunt: ["pupa", "frog", "poring"] }, // android sym,
+};
 
 function parseObjects(gameObjects = []) {
   let player = {},
     mobs = [],
     items = [];
   gameObjects.forEach((e) => {
+    console.log(">>GemBotServer/index::", "obj", e.name, e.status, e.type); //TRACE
     switch (e.type) {
       case "PC":
         player = e;
         break;
       case "MOB":
-        mobs.push(e);
+        if (!e.name.includes("npc")) mobs.push(e);
         break;
       case "ITEM":
         items.push(e);
@@ -29,25 +33,54 @@ function parseObjects(gameObjects = []) {
   return { player, mobs, items };
 }
 
-function getTargetMob(player, mobs) {
+function getTargetMob(player, mobs, deviceId) {
   if (!mobs || mobs.length < 1) return;
-  const list = sortBy(mobs, (mob) => {
-    if (mob.status === "DEAD") return 99999;
-    const dist = getDistance(player, mob);
-    return dist;
-  });
+  if (!DEVICE_MAPPING[deviceId]) return;
+  if (DEVICE_MAPPING[deviceId].hunt.length < 1) return;
+
+  const mobHUnt = new RegExp(DEVICE_MAPPING[deviceId].hunt.join("|"));
+  const list = sortBy(
+    mobs.filter((mob) => {
+      // const isKill = !mob.name.includes("Creamy");
+      const mobNameLower = mob.name.toLowerCase();
+      const isKill = mobHUnt.test(mobNameLower);
+      console.log(
+        ">>GemBotServer/index::",
+        "isKill",
+        isKill,
+        mobNameLower,
+        mobHUnt
+      ); //TRACE
+      return (
+        isKill ||
+        (!isKill && ["TAKEDAMAGE", "BATTLESTANCE", "ATK1"].includes(mob.status))
+      );
+    }),
+    (mob) => {
+      if (mob.attack === "ATK1") return 0;
+      if (mob.status === "DEAD") return 99999;
+      const dist = getDistance(player, mob);
+      return dist;
+    }
+  );
   return list[0];
 }
 
 async function process(data, options) {
+  const deviceId = options.id;
+  if (!DEVICE_MAPPING[deviceId]) {
+    console.log(">>GemBotServer/index::", "NO DEVICE MAPPING FOR ", deviceId); //TRACE
+    return { sleep: 20000 };
+  }
   const gameObjects = JSON.parse(data);
   // console.log(">>GemBotServer/index::", "gameObjects", gameObjects); //TRACE
   const { player, mobs, items } = parseObjects(gameObjects);
+  // console.log(">>GemBotServer/index::", "gameObjects", gameObjects); //TRACE
   // console.log(">>GemBotServer/index::", "player", player); //TRACE
   // return { sleep: 3000 };
   if (player.status === "ATK1") return { sleep: 1000 };
 
-  const targetMob = getTargetMob(player, mobs);
+  const targetMob = getTargetMob(player, mobs, deviceId);
   const targetItems = items;
 
   if (targetItems.length > 0) return { clickAction: true, sleep: 400 };
@@ -64,25 +97,31 @@ async function process(data, options) {
   if (targetItems.length > 0) {
     targetPoint = { x: targetItems[0].x, y: targetItems[0].y };
   }
+
+  // targetPoint =  { x: player.x, y: player.y }
+
   console.log(">>GemBotServer/index::", "player pos", player.x, player.y); //TRACE
 
   let nextPoints;
   let patrolling = false,
     tooFar = false;
+  const mapName = DEVICE_MAPPING[deviceId].map;
+
   if (targetPoint) {
     const targetDistance = getDistance(targetPoint, player);
     console.log(">>GemBotServer/index::", "distance", targetDistance); //TRACE
     // if (targetDistance > 0) {
     nextPoints = pathFinder({
-      mapName: DEFAULT_MAP,
+      mapName,
       from: player,
       to: {
         x: targetPoint.x,
         y: targetPoint.y,
       },
+      deviceId,
       // allowClickSelf: targetItems.length > 0,
     });
-    if (nextPoints.toFar) tooFar = true;
+    if (nextPoints && nextPoints.toFar) tooFar = true;
     // } else {
     //   tooFar = true;
     // }
@@ -90,9 +129,10 @@ async function process(data, options) {
   if (!targetPoint || tooFar) {
     patrolling = true;
     nextPoints = pathFinder({
-      mapName: DEFAULT_MAP,
+      mapName,
       from: player,
       to: "PATROL",
+      deviceId,
     });
   }
 
@@ -145,8 +185,8 @@ app.post("/xy", async function (req, res) {
   // console.log(">>GemBotServer/index::", "hello"); //TRACE
   // return res.send(JSON.stringify({ clickAction: true, sleep: 1000 }));
   let response = {};
-  const options = parseOption(req.query.q2);
-
+  const options = parseOption(req.query.q2); //{"id":"8006baab5f937fd8","height":1280,"width":720}
+  // console.log('>>GemBotServer/index::','req.query', req.query); //TRACE
   if (req.query.q) {
     response = await process(req.query.q, options);
   }
